8f141a19715cf4f9a30fd0af3b810b56c8adb2b7
prerovskysi <prerovsky.si.2020@ssps.cz>
2022-06-15 15:44:38 +0200

    base comit

---
+++ AVILOT/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AVILOT.Models;
+using AVILOT.ViewModels;
+using AVILOT.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AVILOT.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ AVILOT/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AVILOT.Models;
+using AVILOT.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AVILOT.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
