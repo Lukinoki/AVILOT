ed94297ceae8c4e7efd385bf8decf2197f9f7449
VIPTrollik/amd-pc <simon.trom@gmail.com>
2022-06-30 13:58:38 +0200

    added question data model

---
+++ AVILOT/QuestionsEngine/DataModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVILOT.QuestionsEngine
+{
+    internal class DataModels
+    {
+    }
+    public class Question
+    {
+        public int? id { get; set; }
+        public string? question { get; set; }
+
+        public int rightAnswerIndex { get; set; }
+
+        public string[] answers { get; set; }
+        public string correctAnswer { get { return answers[rightAnswerIndex]; } }
+        public DateTime? lastRight { get; set; }
+        public DateTime? lastWrong { get; set; }
+        public string category { get; set; }
+
+        public int wrongs = 0;
+        public int rights = 0;
+    }
+
+}
